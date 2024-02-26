15f480151c564b12d9421c682d302fd91597affc
agrach060 <128651743+agrach060@users.noreply.github.com>
2024-02-25 17:40:34 -0800

    Create task.h

---
+++ task.h
@@ -0,0 +1,23 @@
+/**
+ * Representation of a task in the system.
+ */
+
+#ifndef TASK_H
+#define TASK_H
+
+// representation of a task
+typedef struct task
+{
+    char *name;
+    int tid;
+    int priority;
+    int burst;
+
+    int tat;
+    int wt;
+    int rt;
+    int remainingBurst;
+    bool started;
+} Task;
+
+#endif
